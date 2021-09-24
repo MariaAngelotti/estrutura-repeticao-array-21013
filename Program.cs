f981cc3cbb84011d99976b48b01f39eb8498221f
Maria Júlia <majuadelima@gmail.com>
2021-09-24 14:41:38 -0300

    Alteração do git

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] alunosAMS = new string[5] {
+                "Dani",
+                "Lucas",
+                "Mari",
+                "Bruno",
+                "Maju" };
+
+
+            Console.WriteLine(alunosAMS[3]);
+
+            alunosAMS[3] = "Ramon";
+
+            Console.WriteLine("");
+
+            Console.WriteLine($"Novo valor no index 3 -> {alunosAMS[3]}");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("ForEach");
+
+            Console.WriteLine("");
+
+            foreach (string aluno in alunosAMS)
+            {
+                Console.WriteLine(aluno);
+            }
+            Console.WriteLine("");
+
+            Console.WriteLine("For");
+
+            Console.WriteLine("");
+
+            for (int i = 0; i < alunosAMS.Length; i++)
+            {
+                Console.WriteLine(alunosAMS[i]);
+            }
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+1- git init - (comando para inicializar o git)
+2- git remote add origin https://github.com/MariaAngelotti/estrutura-repeticao-array-21013.git (serve para adicionar um repositório);
+2.1- git remote set-url origin https://github.com/MariaAngelotti/estrutura-repeticao-array-21013.git (serve para alterar um repositório);
+3- git remote -v (comando para visualizar o repositório atual);
+4- git add . (adicionar os arquivos e mudanças preparando para executar um commit - onde "." (ponto) significa todos);
+5- git commit -m "Nome do commit" (commit pacote de alterações);
+6- git push origin (onde branch é o nome da ramificação que estaremos usando, nesse caso usamos a master) git config --global user.email "you@example.com" git config --global user.name "Your name";
